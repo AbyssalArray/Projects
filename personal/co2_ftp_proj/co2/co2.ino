54b1cb560c3ff9fc666c7876327350b30b6fea68
Evgeny Zhilkin <vortxe135@gmail.com>
2024-01-30 11:57:08 +1300

    setup for storing stettings

--- personal/co2_ftp_proj/co2/co2.ino
+++ personal/co2_ftp_proj/co2/co2.ino
@@ -1,18 +1,19 @@
 #include "Arduino.h"
 #include <EEPROM.h>
 #include <WiFi.h>
-#include <WiFiClient.h> 
+#include <WiFiClient.h>
 #include <ESP32_FTPClient.h>
 #include <BMP280.h>
 #include <Wire.h>
 #include "SparkFun_SCD30_Arduino_Library.h"
-#include <Adafruit_GFX.h> 
+#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <splash.h>
+#include <Preferences.h>
 
 
 // 1 Activate and 0 deactivate debug lines
-#define DEBUG  1
+#define DEBUG 1
 #if DEBUG == 1
 #define debug(x) Serial.print(x)
 #define debugln(x) Serial.println(x)
@@ -22,43 +23,52 @@
 #endif
 
 
-#define EEPROM_SIZE 512   //esp32 
+#define EEPROM_SIZE 512  //esp32
+#define M_STR_LEN 64
+#define M_PATH_LEN 200
 
-char wifi_ssid[64]; //63 + 1 for terminator char 
-char wifi_pass[64]; //it is 32 but choosing 63 to fit with the rest of the variables
+typedef struct {
+  char wifi_ssid[M_STR_LEN];  //63 + 1 for terminator char
+  char wifi_pass[M_STR_LEN];  //it is 32 but choosing 63 to fit with the rest of the variables
 
-// couldnt find clear limits so setting to 63 chars
-char ftp_server[64];
-char ftp_user[64];
-char ftp_pass[64];
+  // couldnt find clear limits so setting to 63 chars
+  char ftp_server[M_STR_LEN];
+  char ftp_user[M_STR_LEN];
+  char ftp_pass[M_STR_LEN];
 
-char ftp_path[200];  //gives headroom for file renaming lim 255 
+  char ftp_path[M_PATH_LEN];  //gives headroom for file renaming lim 255
 
-char read_freq = 1;  // readings per hr
+  char read_freq = 1;                                          // readings per hr
+} settings_t;
+settings_t set;
 
-BMP280 bthSen;      //barometer, temperature and humidity sensor
-SCD30 cSen;         // co2 sensor
-ESP32_FTPClient ftp(ftp_server,ftp_user,ftp_pass, 5000, 2);  //initialising here to keep it global for now.
-Adafruit_SSD1306 disp;  // oled display
+BMP280 bthSen;                                               //barometer, temperature and humidity sensor
+SCD30 cSen;                                                    // co2 sensor
+ESP32_FTPClient* ftp = NULL;  //initialising here to keep it global for now.
+Adafruit_SSD1306 disp;                                         // oled display
+Preferences mem;
+
+void load_set();
+void update_set();
+void save_set();
+void check_connect();
 
 void setup() {
+  mem.begin("co2", false);
   Serial.begin(115200);
   delay(10);
+
+  
   Wire.begin();
   disp.begin();
   bthSen.begin();
   cSen.begin();
-
-  if (!EEPROM.begin(EEPROM_SIZE))
-  {
-    debugln("failed to initialise EEPROM"); 
-    // need to add GUI error here
-    while(1) delay(1000000);
-  }
+}
+void save_set() {
+  mem.putBytes("settings", &set, sizeof(set));
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
